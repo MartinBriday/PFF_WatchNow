2b606df251ade9c04e51370d98dbc5ef46bd2ea4
MartinBriday <briday.martin@gmail.com>
2022-06-08 14:21:25 +0200

    Create SpringBoot backend project.

---
+++ backend/WatchNow/src/main/java/com/intiformation/WatchNow/WatchNowApplication.java
@@ -0,0 +1,13 @@
+package com.intiformation.WatchNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchNowApplication.class, args);
+	}
+
+}
---
+++ backend/WatchNow/src/test/java/com/intiformation/WatchNow/WatchNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intiformation.WatchNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
