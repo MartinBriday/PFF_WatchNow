d81e3d4a39ecaac2363b09c9779d55162218989e
rigol <rigolet.antoine@gmail.com>
2022-06-09 11:58:13 +0200

    Ajout des classes

---
+++ backend/WatchNow/src/main/java/com/intiformation/WatchNow/model/Avis.java
@@ -0,0 +1,32 @@
+package com.intiformation.WatchNow.model;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Avis {
+
+    /**
+     * Default constructor
+     */
+    public Avis() {
+    }
+
+    /**
+     * 
+     */
+    private int id;
+
+    /**
+     * 
+     */
+    private double note;
+
+    /**
+     * 
+     */
+    private String commentaire;
+
+}
---
+++ backend/WatchNow/src/main/java/com/intiformation/WatchNow/model/Serie.java
@@ -0,0 +1,17 @@
+package com.intiformation.WatchNow.model;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Serie extends Oeuvre {
+
+    /**
+     * Default constructor
+     */
+    public Serie() {
+    }
+
+}
