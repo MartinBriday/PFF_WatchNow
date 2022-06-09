2fef44aa9ee8ec8a4455333b0bfeef05b47c0600
rigol <rigolet.antoine@gmail.com>
2022-06-09 12:26:36 +0200

    Ajout des annotations lombok dans les 8 classes attribuées à Antoine (cf Notion) @Entity, @Table, @Data, @Id et @GeneratedValue + modification des noms de variables

--- backend/WatchNow/src/main/java/com/intiformation/WatchNow/model/Admin.java
+++ backend/WatchNow/src/main/java/com/intiformation/WatchNow/model/Admin.java
@@ -3,14 +3,16 @@ package com.intiformation.WatchNow.model;
 
 import java.util.*;
 
-/**
- * 
- */
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table
+@Data
 public class Admin extends Utilisateur {
 
-    /**
-     * Default constructor
-     */
     public Admin() {
     }
 
--- backend/WatchNow/src/main/java/com/intiformation/WatchNow/model/Avis.java
+++ backend/WatchNow/src/main/java/com/intiformation/WatchNow/model/Avis.java
@@ -3,30 +3,26 @@ package com.intiformation.WatchNow.model;
 
 import java.util.*;
 
-/**
- * 
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table
+@Data
 public class Avis {
 
-    /**
-     * Default constructor
-     */
     public Avis() {
     }
 
-    /**
-     * 
-     */
-    private int id;
-
-    /**
-     * 
-     */
-    private double note;
-
-    /**
-     * 
-     */
-    private String commentaire;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int _id;
+    private double _note;
+    private String _commentaire;
 
 }
