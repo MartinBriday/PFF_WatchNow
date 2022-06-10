08e0eee1e37a8976a4fd2c6f409c74b3c3cc2b4f
MartinBriday <briday.martin@gmail.com>
2022-06-10 15:17:52 +0200

    Cross requests between two APIs, working yet.

---
+++ backend/WatchNow/src/main/java/com/intiformation/WatchNow/model/OeuvreBuffer.java
@@ -0,0 +1,13 @@
+package com.intiformation.WatchNow.model;
+
+public class OeuvreBuffer {
+	private String imdbid;
+
+	public String getID() {
+		return imdbid;
+	}
+
+	public void setID(String imdbid) {
+		this.imdbid = imdbid;
+	}
+}
--- backend/WatchNow/src/main/java/com/intiformation/WatchNow/service/OeuvreServiceImpl.java
+++ backend/WatchNow/src/main/java/com/intiformation/WatchNow/service/OeuvreServiceImpl.java
@@ -1,8 +1,11 @@
 package com.intiformation.WatchNow.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.intiformation.WatchNow.model.Oeuvre;
+import com.intiformation.WatchNow.model.OeuvreBuffer;
+import com.intiformation.WatchNow.model.OeuvreBufferResult;
 import com.intiformation.WatchNow.model.OeuvreResultObject;
 
 import org.springframework.stereotype.Service;
@@ -11,8 +14,10 @@ import kong.unirest.Unirest;
 
 @Service
 public class OeuvreServiceImpl implements OeuvreService {
-	
-	private static final String requestURL = "https://streaming-availability.p.rapidapi.com/";
+
+	private static final String requestURL_Streaming_Availability = "https://streaming-availability.p.rapidapi.com/";
+	private static final String requestURL_mdblist = "https://mdblist.p.rapidapi.com/";
+	private static final String rapidAPI_key = "f8c66a27d0msh5fb0123b6da76c7p12ef0cjsna74230c7548d";
 	
 
 //	@Override
@@ -27,10 +32,10 @@ public class OeuvreServiceImpl implements OeuvreService {
 	
 	@Override
 	public Oeuvre getById(String id) {
-		Oeuvre resultRequest;
-		resultRequest = Unirest.get(requestURL + "get/basic?country=us&output_language=en&imdb_id=" + id)
+		Oeuvre resultRequest = null;
+		resultRequest = Unirest.get(requestURL_Streaming_Availability + "get/basic?country=us&output_language=en&imdb_id=" + id)
 		    .header("X-RapidAPI-Host", "streaming-availability.p.rapidapi.com")
-		    .header("X-RapidAPI-Key", "f69fe30c89msh67c47b9daafa953p14b7cdjsn133e63e7f94c")
+		    .header("X-RapidAPI-Key", rapidAPI_key)
 		    .asObject(Oeuvre.class)
             .getBody();
 		return resultRequest;
@@ -39,8 +44,17 @@ public class OeuvreServiceImpl implements OeuvreService {
 
 	@Override
 	public List<Oeuvre> getByTitre(String titre) {
-		
-		return null;
+		List<Oeuvre> listOeuvre = new ArrayList<Oeuvre>();
+		OeuvreBufferResult resultRequest = null;
+		resultRequest = Unirest.get(requestURL_mdblist + "?s=" + titre)
+			.header("X-RapidAPI-Key", rapidAPI_key)
+			.header("X-RapidAPI-Host", "mdblist.p.rapidapi.com")
+		    .asObject(OeuvreBufferResult.class)
+            .getBody();
+		for (OeuvreBuffer _ob : resultRequest.getSearch()) {
+			listOeuvre.add(getById(_ob.getID()));
+		}
+		return listOeuvre;
 	}
 
 	
