1a28ccbb350ca3f7dad873b9ae8c7f1cd85103ff
rigol <rigolet.antoine@gmail.com>
2022-06-10 12:56:40 +0200

    Ajout des mapping dans les controllers Admin, Avis, Langue, Nationalite et Realisateur

--- backend/WatchNow/src/main/java/com/intiformation/WatchNow/controller/AdminController.java
+++ backend/WatchNow/src/main/java/com/intiformation/WatchNow/controller/AdminController.java
@@ -1,8 +1,19 @@
 package com.intiformation.WatchNow.controller;
 
+import java.util.List;
+
+import com.intiformation.WatchNow.model.Admin;
 import com.intiformation.WatchNow.service.AdminService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +24,29 @@ public class AdminController
 	@Autowired
 	AdminService adminService;
 	
-//	@GetMapping("/index")
+	@GetMapping("/index")
+	public ResponseEntity<List<Admin>> getListAdmin()
+	{
+		return new ResponseEntity<List<Admin>>(adminService.get(), HttpStatus.OK);
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<Admin> saveAdmin(@RequestBody Admin n)
+	{
+		return new ResponseEntity<Admin>(adminService.save(n), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/update")
+	public String updateAdmin(@RequestBody Admin n)
+	{
+		adminService.save(n);
+		return "redirect:/index";
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteAdmin(@PathVariable("id") int id)
+	{
+		adminService.delete(id);
+		return "redirect:/index";
+	}
 }
--- backend/WatchNow/src/main/java/com/intiformation/WatchNow/controller/AvisController.java
+++ backend/WatchNow/src/main/java/com/intiformation/WatchNow/controller/AvisController.java
@@ -8,8 +8,12 @@ import com.intiformation.WatchNow.service.AvisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +24,29 @@ public class AvisController {
 	@Autowired
 	private AvisService avisService;
 	
-//	@GetMapping("/index/{id}")
-//	public ResponseEntity<List<Avis>> getAvisById(@PathVariable String id) {
-//		return new ResponseEntity<List<Avis>>(avisService.getByOeuvreId(id), HttpStatus.OK);
-//	}
+	@GetMapping("/index")
+	public ResponseEntity<List<Avis>> getListAvis()
+	{
+		return new ResponseEntity<List<Avis>>(avisService.get(), HttpStatus.OK);
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<Avis> saveAvis(@RequestBody Avis n)
+	{
+		return new ResponseEntity<Avis>(avisService.save(n), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/update")
+	public String updateAvis(@RequestBody Avis n)
+	{
+		avisService.save(n);
+		return "redirect:/index";
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteAvis(@PathVariable("id") int id)
+	{
+		avisService.delete(id);
+		return "redirect:/index";
+	}
 }
