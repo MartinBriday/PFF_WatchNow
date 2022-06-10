3826b97b1459811b4419cfffdca034e2298d13ff
Melissa-intiformation <melissa.z2101@gmail.com>
2022-06-10 17:07:11 +0200

    Ajout du début de la navbar

--- frontend/WatchNow/src/app/app-routing.module.ts
+++ frontend/WatchNow/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './page/accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"accueil", component: AccueilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
