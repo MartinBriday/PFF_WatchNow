25e71f7173aee6aee32a72d0322ea2a974a81cb6
Melissa-intiformation <melissa.z2101@gmail.com>
2022-06-13 12:47:00 +0200

    début de la carousel

--- frontend/WatchNow/src/app/app-routing.module.ts
+++ frontend/WatchNow/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListeAleatoireFilmComponent } from './composant/liste-aleatoire-film/liste-aleatoire-film.component';
 import { AccueilComponent } from './page/accueil/accueil.component';
 
 const routes: Routes = [
-  {path:"accueil", component: AccueilComponent}
+  {path:"accueil", component: AccueilComponent, children: [
+    {path:"section2", component: ListeAleatoireFilmComponent}
+  ]}
 ];
 
 @NgModule({
