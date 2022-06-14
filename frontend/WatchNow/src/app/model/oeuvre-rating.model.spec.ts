93f3787f277c5eee5291764aa855d0256311b343
Melissa-intiformation <melissa.z2101@gmail.com>
2022-06-14 13:01:53 +0200

    Ajout des model OeuvreImage et OeuvreTitle dans le front

---
+++ frontend/WatchNow/src/app/model/oeuvre-rating.model.spec.ts
@@ -0,0 +1,7 @@
+import { OeuvreRating } from './oeuvre-rating.model';
+
+describe('OeuvreRating', () => {
+  it('should create an instance', () => {
+    expect(new OeuvreRating()).toBeTruthy();
+  });
+});
