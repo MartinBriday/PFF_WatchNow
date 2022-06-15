d9025bd015d9a6b1e2326d4abf18d82f157c2a98
MartinBriday <briday.martin@gmail.com>
2022-06-15 17:21:26 +0200

    Add OeuvreWatch class and affiliates implementing the ways to watch oeuvres.

---
+++ backend/WatchNow/src/main/java/com/intiformation/WatchNow/service/OeuvreWatchService.java
@@ -0,0 +1,8 @@
+package com.intiformation.WatchNow.service;
+
+import com.intiformation.WatchNow.model.OeuvreWatch;
+
+public interface OeuvreWatchService {
+
+	OeuvreWatch getByOeuvreId(String id);
+}
