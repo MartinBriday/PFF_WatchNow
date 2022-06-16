8d6794cdaa59a60c7f1d44d6b3c2dea8f952c391
rigol <rigolet.antoine@gmail.com>
2022-06-16 14:15:48 +0200

    Form basique + app-routing

--- frontend/WatchNow/src/app/app-routing.module.ts
+++ frontend/WatchNow/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { FormInscriptionComponent } from './page/form-inscription/form-inscription.component';
 import { MonCompteComponent } from './page/mon-compte/mon-compte.component';
 import { CategorieFilmComponent } from './page/categorie-film/categorie-film.component';
 import { NgModule } from '@angular/core';
@@ -11,6 +12,7 @@ import { ResultatRechercheComponent } from './page/resultat-recherche/resultat-r
 
 const routes: Routes = [
   {path:"accueil", component: AccueilComponent},
+  {path:"form", component: FormInscriptionComponent},
   {path:"categorie-film", component: CategorieFilmComponent},
   {path:"recherche/:searchWord", component: ResultatRechercheComponent},
   {path:"film/:id", component: FilmComponent, children:[{path: "synopsis/:id", component: SynopsisComponent}]},
