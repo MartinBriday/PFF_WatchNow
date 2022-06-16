86c293fc073507b32fe28f1f28240fbdac7ac4e4
rigol <rigolet.antoine@gmail.com>
2022-06-16 12:49:28 +0200

    Ajout pour la page "mon-compte" des fonctions pour afficher les carousels "à regarder plus tard", "déjà vu" et "en cours" pour séries et films

--- frontend/WatchNow/src/app/page/mon-compte/mon-compte.component.ts
+++ frontend/WatchNow/src/app/page/mon-compte/mon-compte.component.ts
@@ -1,3 +1,9 @@
+import { SerieService } from 'src/app/service/serie.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Film } from 'src/app/model/film.model';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { FilmService } from 'src/app/service/film.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +12,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mon-compte.component.css']
 })
 export class MonCompteComponent implements OnInit {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    margin: 20,
+    stagePadding: 20,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+  constructor(private filmService: FilmService, private serieService: SerieService, private router: Router) { }
 
-  constructor() { }
+  listSeriesEnCours$!: Observable<Film[]>;
+  listFilmsEnCours$!: Observable<Film[]>;
+  listSeriesLater$!: Observable<Film[]>;
+  listFilmsLater$!: Observable<Film[]>;
+  listSeriesVu$!: Observable<Film[]>;
+  listFilmsVu$!: Observable<Film[]>;
 
   ngOnInit(): void {
+    this.listSeriesEnCours$ = this.serieService.getlistSeriesEnCours();
+    this.listFilmsEnCours$ = this.filmService.getlistFilmsEnCours();
+
+    this.listSeriesLater$ = this.serieService.getlistSeriesLater();
+    this.listFilmsLater$ = this.filmService.getlistFilmsLater();
+
+    this.listSeriesVu$ = this.serieService.getlistSeriesVu();
+    this.listFilmsVu$ = this.filmService.getlistFilmsVu();
   }
 
 }
