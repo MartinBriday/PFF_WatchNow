94506ea828dfe04478307e0bf0ff3aa233fbb42f
rigol <rigolet.antoine@gmail.com>
2022-06-17 12:07:16 +0200

    Ajouts multiples

--- frontend/WatchNow/src/app/app-routing.module.ts
+++ frontend/WatchNow/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CategorieSerieComponent } from './page/categorie-serie/categorie-serie.component';
 import { FormInscriptionComponent } from './page/form-inscription/form-inscription.component';
 import { MonCompteComponent } from './page/mon-compte/mon-compte.component';
 import { CategorieFilmComponent } from './page/categorie-film/categorie-film.component';
@@ -17,6 +18,7 @@ const routes: Routes = [
   {path:"accueil", component: AccueilComponent},
   {path:"inscription", component: FormInscriptionComponent},
   {path:"categorie-film", component: CategorieFilmComponent},
+  {path:"categorie-serie", component: CategorieSerieComponent},
   {path:"recherche/:searchWord", component: ResultatRechercheComponent},
   {path:"rechercheAvancee", component: RechercheAvanceeComponent},
   {path:"film/:id", component: FilmComponent, children:[{path: "synopsis/:id", component: SynopsisComponent}]},
